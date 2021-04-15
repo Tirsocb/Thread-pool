47c31a13fa5bfe9622b75e282a13b8685160ae8a
tirso <tirso@ufm.edu>
2021-04-15 16:51:54 -0600

    finished parallelism and multiplication

--- matmul.py
+++ matmul.py
@@ -1,9 +1,8 @@
 import csv
 import sys
 import time as t
-import threading
+from threading import Thread
 import numpy as np
-
 
 # captura de argumentos en linea de comandos
 print('Number of arguments:', len(sys.argv))
@@ -21,20 +20,42 @@ except:
     print("\nto retry: python3 matmul.py <matrixA.csv> <matrixB.csv> <Pool size (int)> <OutputFile.txt>")
 fileO = sys.argv[4]
 
+# obtener dimensiones de la matriz (columnas)
+n = int(matrixA.shape[0])
+# crear matriz para guardar resultado
+matrixC = np.zeros((n, n), int)
+
+print(n)
+
 print("MATRIX A")
 print(matrixA)
 
 print("\n MATRIX B")
 print(matrixB)
 
-print("\n THREADS: "+ str(poolSize))
+print("\n THREADS: " + str(poolSize))
+
+
+def Multiplication(s, e):
+    for i in range(s, e):
+        print(str(i))
+        for j in range(n):
+            for k in range(n):
+                matrixC[i][j] += int(matrixA[i][k] * matrixB[k][j])
+
+
+def parallel(size):
+    threadHandler = []
+    for i in range(0, size):
+        thread = Thread(target=Multiplication, args=(int((n / size) * i), int((n / size) * (i + 1))))
+        threadHandler.append(thread)
+        thread.start()
+
+    for j in range(0, size):
+        threadHandler[j].join()
 
 
 
+parallel(poolSize)
 
-
-
-
-
-
-
+print(matrixC)
